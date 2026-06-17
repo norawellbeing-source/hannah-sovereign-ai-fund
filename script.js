0d6985ef25bafd9b6f3e9e4bf5094976a083080d
Nora <norawellbeing@gmail.com>
2026-06-17 11:56:29 +0100

    Add accordion functionality to script.js

--- script.js
+++ script.js
@@ -10,3 +10,23 @@ const observer = new IntersectionObserver((entries) => {
 }, { threshold: 0.15 });
 
 fadeEls.forEach((el) => observer.observe(el));
+
+const accordionItems = document.querySelectorAll('.accordion-item');
+
+accordionItems.forEach((item) => {
+  const trigger = item.querySelector('.accordion-trigger');
+
+  trigger.addEventListener('click', () => {
+    const isOpen = item.classList.contains('is-open');
+
+    accordionItems.forEach((other) => {
+      other.classList.remove('is-open');
+      other.querySelector('.accordion-trigger').setAttribute('aria-expanded', 'false');
+    });
+
+    if (!isOpen) {
+      item.classList.add('is-open');
+      trigger.setAttribute('aria-expanded', 'true');
+    }
+  });
+});
